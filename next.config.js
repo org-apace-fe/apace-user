c2fcd1ec216c144dcfeac9b8d9ce6722244b44bc
Damilare Ademeso <ademesodamilare@gmail.com>
2021-12-31 10:32:00 +0100

    help Lord

--- next.config.js
+++ next.config.js
@@ -5,4 +5,9 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    loader: "cloudinary",
+    path: "https://orgapace.blob.core.windows.net",
+    domains: ["images.pexels.com", "https://picsum.photos/", "useapace.com"],
+  },
 };
